a07ab1efa4dfae8a259bc6f7118f079989228d91
nitram <ndungu.nitram@gmail.com>
2025-09-22 00:12:41 +0300

    Update configs and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/cbk_retail_bonds/',   // 👈 important for GitHub Pages
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
